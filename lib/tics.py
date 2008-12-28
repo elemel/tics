a92af4ed1683229f1769e8bb2875effb247bcf3b
Mikael Lind <elemel@elemel.se>
2008-12-28 22:45:09 +0100

    evolve image

--- lib/tics.py
+++ lib/tics.py
@@ -8,10 +8,12 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 import pygame, random, sys, numpy
 from pygame.locals import *
+from itertools import count, chain, izip
 
 class Triangle(object):
     def __init__(self, corners):
-        self.__corners = tuple(tuple(c) for c in corners)
+        self.__corners = tuple(tuple(float(comp) for comp in corner)
+                               for corner in corners)
 
     def draw(self):
         glBegin(GL_TRIANGLES)
@@ -30,9 +32,13 @@ class Image(object):
     def draw(self):
         for triangle in self.__triangles:
             triangle.draw()
-
+    
     def __iter__(self):
         return iter(self.__triangles)
+        
+    @property
+    def triangles(self):
+        return self.__triangles
 
 def init():
     glShadeModel(GL_SMOOTH)
@@ -50,9 +56,22 @@ def generate_triangle(random=random):
 def generate_image(triangle_count, random=random):
     return Image([generate_triangle() for _ in xrange(triangle_count)])
 
+def mutate_triangle(triangle, random=random):
+    new_triangle = generate_triangle(random)
+    weight = random.random()
+    return Triangle(((comp * weight + new_comp * (1 - weight))
+                     for comp, new_comp in izip(corner, new_corner))
+                    for corner, new_corner in izip(triangle, new_triangle))
+
+def mutate_image(image, random=random):
+    triangles = image.triangles
+    i = random.randrange(len(triangles))
+    triangle = mutate_triangle(triangles[i], random)
+    return Image(chain(triangles[:i], [triangle], triangles[i + 1:]))
+
 def get_pixels(surface):
     width, height = surface.get_size()
-    return numpy.array([[surface.get_at((x, height - y - 1))
+    return numpy.array([[surface.get_at((x, height - y - 1))[:3]
                          for y in xrange(height)]
                         for x in xrange(width)])
 
@@ -67,17 +86,22 @@ def main():
     goal_pixels = get_pixels(goal_surface)
     pygame.display.set_mode(size, OPENGL | DOUBLEBUF)
     init()
-    while True:
+    best_image = generate_image(32)
+    best_fitness = 1
+    for generation in count():
         event = pygame.event.poll()
         if (event.type == QUIT or
             (event.type == KEYDOWN and event.key == K_ESCAPE)):
             break
-        image = generate_image(64)
+        image = mutate_image(best_image)
         redraw(image)
         pygame.display.flip()
-        image_pixels = glReadPixels(0, 0, width, height, GL_RGBA, GL_BYTE)
+        image_pixels = glReadPixels(0, 0, width, height, GL_RGB, GL_BYTE)
         fitness = ((image_pixels - goal_pixels) ** 2).mean() / 255 ** 2
-        print fitness
+        if fitness < best_fitness:
+            print "#%d: %.7f" % (generation, fitness)
+            best_image = image
+            best_fitness = fitness
 
 if __name__ == '__main__':
     main()
