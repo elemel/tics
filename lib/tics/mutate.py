834c454c14544d28b07e479030d001dc28f70575
Mikael Lind <elemel@elemel.se>
2008-12-29 15:11:32 +0100

    renamed adjust mutation to nudge

--- lib/tics/mutate.py
+++ lib/tics/mutate.py
@@ -4,18 +4,18 @@ from tics.generate import generate_triangle
 from tics.image import Image
 from tics.triangle import Triangle
 
-def adjust(image, random=random):
+def bubble(image, random=random):
+    triangles = image.triangles
+    i = random.randrange(len(triangles) - 1)
+    triangles[i], triangles[i + 1] = triangles[i + 1], triangles[i]
+
+def nudge(image, random=random):
     triangle = random.choice(image.triangles)
     for vertex in triangle.vertices:
         comps = [vertex.x, vertex.y, vertex.r, vertex.g, vertex.b, vertex.a]
         i = random.randrange(len(comps))
         comps[i] = random.normalvariate(comps[i], 0.1)
         vertex.x, vertex.y, vertex.r, vertex.g, vertex.b, vertex.a = comps
-
-def bubble(image, random=random):
-    triangles = image.triangles
-    i = random.randrange(len(triangles) - 1)
-    triangles[i], triangles[i + 1] = triangles[i + 1], triangles[i]
 
 def replace(image, random=random):
     i = random.randrange(len(image.triangles))
@@ -31,6 +31,6 @@ def swap(image, random=random):
     triangles[i], triangles[j] = triangles[j], triangles[i]
 
 def mutate_image(image, random=random):
-    mutations = [adjust, bubble, replace, shuffle, swap]
+    mutations = [bubble, nudge, replace, shuffle, swap]
     mutation = random.choice(mutations)
     mutation(image, random)
