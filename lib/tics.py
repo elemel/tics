90d4f38dbc21bdf14bf68f317fbd46acdefa99e1
Mikael Lind <elemel@elemel.se>
2008-12-29 10:23:18 +0100

    extract module triangle

--- lib/tics.py
+++ lib/tics.py
@@ -9,26 +9,7 @@ from OpenGL.GLU import *
 import pygame, random, sys, numpy
 from pygame.locals import *
 from itertools import count, chain, izip
-
-def normalize_comp(comp):
-    return float(max(0, min(comp, 1)))
-
-def normalize_corner(corner):
-    return tuple(normalize_comp(comp) for comp in corner)
-
-class Triangle(object):
-    def __init__(self, corners):
-        self.__corners = tuple(normalize_corner(corner) for corner in corners)
-
-    def draw(self):
-        glBegin(GL_TRIANGLES)
-        for r, g, b, a, x, y in self:
-            glColor(r, g, b, a)
-            glVertex(-1 + 2 * x, -1 + 2 * y)
-        glEnd()
-        
-    def __iter__(self):
-        return iter(self.__corners)
+from tics.triangle import Triangle
 
 class Image(object):
     def __init__(self, triangles):
---
+++ lib/tics/triangle.py
@@ -0,0 +1,22 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+def normalize_comp(comp):
+    return float(max(0, min(comp, 1)))
+
+def normalize_corner(corner):
+    return tuple(normalize_comp(comp) for comp in corner)
+
+class Triangle(object):
+    def __init__(self, corners):
+        self.__corners = tuple(normalize_corner(corner) for corner in corners)
+
+    def draw(self):
+        glBegin(GL_TRIANGLES)
+        for r, g, b, a, x, y in self:
+            glColor(r, g, b, a)
+            glVertex(-1 + 2 * x, -1 + 2 * y)
+        glEnd()
+        
+    def __iter__(self):
+        return iter(self.__corners)
