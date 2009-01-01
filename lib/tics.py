99dc3afc52786903b8c6a9b4876d9cf05374a53d
Mikael Lind <elemel@elemel.se>
2009-01-01 20:17:24 +0100

    yet another (almost) complete rewrite

--- lib/tics.py
+++ lib/tics.py
@@ -2,14 +2,15 @@
 
 # Adapted from Paul Furber's Python version of Nehe's OpenGL Lesson 3.
 
-from __future__ import division
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import pygame, random, sys, numpy, copy
 from pygame.locals import *
-from itertools import count
+from itertools import count, chain
 from operator import itemgetter
+
+TRIANGLE_COUNT = 100
+ALPHA_SCALE = 0.5
 
 def init_opengl():
     glShadeModel(GL_SMOOTH)
@@ -17,69 +18,80 @@ def init_opengl():
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-def render_individual(individual):
+def draw(image):
     glClear(GL_COLOR_BUFFER_BIT)
     glBegin(GL_TRIANGLES)
-    for triangle in sorted(individual, key=itemgetter(0)):
-        for i in xrange(1, len(triangle), 6):
+    for triangle in image:
+        for i in xrange(0, len(triangle), 6):
             x, y, r, g, b, a = triangle[i:i + 6]
-            glColor(r, g, b, a)
-            glVertex(-1 + 2 * x, -1 + 2 * y)
+            glColor4d(r, g, b, a * ALPHA_SCALE)
+            glVertex2d(-1 + 2 * x, -1 + 2 * y)
     glEnd()
     pygame.display.flip()
 
-def get_surface_pixels(surface):
+def pixels_from_surface(surface):
     width, height = surface.get_size()
     return numpy.array([[surface.get_at((x, height - y - 1))[:3]
-                         for y in xrange(height)]
-                        for x in xrange(width)])
+                         for x in xrange(width)] for y in xrange(height)])
 
-def get_fitness(image_pixels, goal_pixels):
-    return ((image_pixels - goal_pixels) ** 2).mean()
+def surface_from_pixels(pixels):
+    width, height = len(pixels), len(pixels[0])
+    surface = pygame.surface.Surface((width, height))
+    for x, column in enumerate(pixels):
+        for y, color in enumerate(column):
+            surface.set_at((x, y), color)
+    return surface
+    
+def fitness(pixels, goal_pixels):
+    return ((pixels - goal_pixels) ** 2).mean()
 
 def generate_triangle(random):
-    return tuple(random.random() for _ in xrange(19))
+    triangle = []
+    color = [random.random() for _ in xrange(4)]
+    x = random.random()
+    y = random.random()
+    r = random.random() ** 3
+    for _ in xrange(3):
+        triangle.append(clamp(x + random.choice([-1, 1]) * r *
+                              random.random()))
+        triangle.append(clamp(y + random.choice([-1, 1]) * r *
+                              random.random()))
+        triangle.extend(color)
+    return triangle
 
-def generate_individual(triangle_count, random):
-    return [generate_triangle(random) for _ in xrange(triangle_count)]
+def clamp(comp):
+    return max(0.0, min(comp, 1.0))
 
-def generate_population(individual_count, triangle_count, random):
-    return [generate_individual(triangle_count, random)
-            for _ in xrange(individual_count)]
+def tweak_comp(comp, random):
+    return clamp(comp + random.choice([-1, 1]) * random.random() ** 3)
 
-def crossover(mother, father, random):
-    assert len(mother) == len(father)
-    i = random.randrange(len(mother) + 1)
-    j = random.randrange(len(mother) + 1)
-    i, j = min(i, j), max(i, j)
-    return mother[:i] + father[i:j] + mother[j:]
+def move_triangle(image, random):
+    i = random.randrange(len(image))
+    j = random.randrange(len(image))
+    triangle = image.pop(i)
+    image.insert(j, triangle)
 
-def select_parent(population, random):
-    i = int(random.random() ** 2 * len(population))
-    return population[i]
+def replace_triangle(image, random):
+    i = random.randrange(len(image))
+    j = random.randrange(len(image))
+    triangle = generate_triangle(random)
+    image.pop(i)
+    image.insert(j, triangle)
 
-def mutate(child, random):
-    i = random.randrange(len(child))
-    triangle = list(child[i])
-    j = random.randrange(len(triangle))
-    triangle[j] += random.choice([-1, 1]) * random.random() ** 2
-    if not (0 <= triangle[j] < 1):
-        triangle[j] = random.random()
-    child[i] = tuple(triangle)
+def tweak_triangle(image, random):
+    triangle = random.choice(image)
+    i = random.randrange(len(triangle))
+    triangle[i] = tweak_comp(triangle[i], random)
+    
+def mutate(image, random):
+    mutation = random.choice([move_triangle, replace_triangle, tweak_triangle])
+    mutation(image, random)
 
-def evolve_population(old_population, fitnesses, random):
-    def cmp_fitness(a, b):
-        return cmp(fitnesses[id(a)], fitnesses[id(b)])
-    old_population.sort(cmp=cmp_fitness)
-    print fitnesses[id(old_population[0])]
-    new_population = [old_population[0]]
-    while len(new_population) < len(old_population):
-        mother = select_parent(old_population, random)
-        father = select_parent(old_population, random)
-        child = crossover(mother, father, random)
-        mutate(child, random)
-        new_population.append(child)
-    return new_population
+def pixels_from_display(width, height):
+    pixels = glReadPixelsub(0, 0, max(width, height), max(width, height),
+                            GL_RGB)
+    assert width >= height
+    return pixels[:height]
 
 def main():
     args = sys.argv[1:]
@@ -89,21 +101,29 @@ def main():
     pygame.init()
     goal_surface = pygame.image.load(args[0])
     width, height = size = goal_surface.get_size()
-    goal_pixels = get_surface_pixels(goal_surface)
+    goal_pixels = pixels_from_surface(goal_surface)
     pygame.display.set_mode(size, OPENGL | DOUBLEBUF)
     init_opengl()
-    population = generate_population(64, 64, random)
+    parent = [generate_triangle(random) for _ in xrange(TRIANGLE_COUNT)]
+    parent_fitness = float("inf")
     for generation in count():
-        fitnesses = {}
         for event in pygame.event.get():
             if (event.type == QUIT or
                 (event.type == KEYDOWN and event.key == K_ESCAPE)):
-                break
-        for individual in population:
-            render_individual(individual)
-            image_pixels = glReadPixels(0, 0, width, height, GL_RGB, GL_BYTE)
-            fitnesses[id(individual)] = get_fitness(image_pixels, goal_pixels)
-        population = evolve_population(population, fitnesses, random)
-
+                pixels = pixels_from_display(width, height)
+                pygame.image.save(surface_from_pixels(pixels), "tics.bmp")
+                sys.exit(0)
+        child = copy.deepcopy(parent)
+        mutate(child, random)
+        draw(child)
+        child_pixels = pixels_from_display(width, height)
+        child_fitness = fitness(child_pixels, goal_pixels)
+        if child_fitness < parent_fitness:
+            parent = child
+            parent_fitness = child_fitness
+        print ("g = %d, t = %d, f = %d"
+               % (generation, pygame.time.get_ticks() / 1000.0,
+                  parent_fitness))
+                  
 if __name__ == '__main__':
     main()
