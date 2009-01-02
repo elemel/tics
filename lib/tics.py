97f014357119b9dcf1b19dfc7e209cd2907a54d4
Mikael Lind <elemel@elemel.se>
2009-01-02 22:35:03 +0100

    custom binary file format

--- lib/tics.py
+++ lib/tics.py
@@ -23,17 +23,11 @@
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import pygame, random, sys, numpy, copy, os
+import pygame, random, sys, numpy, copy, os, struct
 from pygame.locals import *
 
 def log(message):
     sys.stderr.write("tics: %s\n" % message)
-
-try:
-    import cPickle as pickle
-except:
-    log("failed to import cPickle, trying pickle instead")
-    import pickle
 
 TRIANGLE_COUNT = 100
 ALPHA_SCALE = 0.5
@@ -119,6 +113,41 @@ def pixels_from_display(width, height):
     assert width >= height
     return pixels[:height]
 
+def load_triangle_image(path):
+    f = open(path, "rb")
+    width, height, count = struct.unpack("!HHH", f.read(6))
+    image = []
+    for _ in xrange(count):
+        triangle = []
+        for _ in xrange(3):
+            x, y, r, g, b, a = struct.unpack("!HHBBBB", f.read(8))
+            x /= 65535.0
+            y /= 65535.0
+            r /= 255.0
+            g /= 255.0
+            b /= 255.0
+            a /= 255.0
+            triangle.extend([x, y, r, g, b, a])
+        image.append(triangle)
+    f.close()
+    return image, (width, height)
+    
+def save_triangle_image(image, resolution, path):
+    f = open(path, "wb")
+    width, height = resolution
+    f.write(struct.pack("!HHH", width, height, len(image)))
+    for triangle in image:
+        for i in xrange(0, len(triangle), 6):
+            x, y, r, g, b, a = triangle[i:i + 6]
+            x = min(int(x * 65536), 65535)
+            y = min(int(y * 65536), 65535)
+            r = min(int(r * 256), 255)
+            g = min(int(g * 256), 255)
+            b = min(int(b * 256), 255)
+            a = min(int(a * 256), 255)
+            f.write(struct.pack("!HHBBBB", x, y, r, g, b, a))
+    f.close()
+
 def main():
     args = sys.argv[1:]
     if len(args) != 1:
@@ -138,9 +167,10 @@ def main():
     pygame.display.set_mode(size, OPENGL | DOUBLEBUF)
     init_opengl()
     try:
-        parent = pickle.load(open(target_path, "r"))
+        parent, parent_resolution = load_triangle_image(target_path)
     except:
         parent = [generate_triangle(random) for _ in xrange(TRIANGLE_COUNT)]
+        parent_resolution = None
     draw(parent)
     parent_pixels = pixels_from_display(width, height)
     parent_fitness = fitness(parent_pixels, source_pixels)
@@ -150,8 +180,7 @@ def main():
             if (event.type == QUIT or
                 (event.type == KEYDOWN and event.key == K_ESCAPE)):
                 try:
-                    pickle.dump(parent, open(target_path, "w"),
-                                pickle.HIGHEST_PROTOCOL)
+                    save_triangle_image(parent, size, target_path)
                 except:
                     log("could not save target file: %s" % target_path)
                     sys.exit(1)
