76289a6be5f243fbfb9b2645134c76ec4083ad98
Mikael Lind <elemel@elemel.se>
2009-01-07 16:06:33 +0100

    convert original image to 32 bit format

--- src/tics.cpp
+++ src/tics.cpp
@@ -5,7 +5,11 @@
 #include <GL/gl.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
+#include <boost/shared_array.hpp>
+#include <boost/shared_ptr.hpp>
 
+using boost::shared_array;
+using boost::shared_ptr;
 using std::cerr;
 using std::endl;
 using tics::Image;
@@ -39,6 +43,32 @@ namespace {
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         return true;
     }
+    
+    shared_ptr<SDL_Surface> convert_surface_32(SDL_Surface &src)
+    {
+        SDL_PixelFormat fmt;
+        fmt.palette = 0;
+        fmt.BitsPerPixel = 32;
+        fmt.BytesPerPixel = 4;
+        fmt.Rmask = 0xff000000;
+        fmt.Gmask = 0x00ff0000;
+        fmt.Bmask = 0x0000ff00;
+        fmt.Amask = 0x000000ff;
+        fmt.Rloss =  fmt.Gloss = fmt.Bloss = fmt.Aloss = 0;
+        fmt.Rshift = 24;
+        fmt.Gshift = 16;
+        fmt.Bshift = 8;
+        fmt.Ashift = 0;
+        fmt.colorkey = 0;
+        fmt.alpha = 255;
+        return shared_ptr<SDL_Surface>(SDL_ConvertSurface(&src, &fmt, 0),
+                                       SDL_FreeSurface);
+    }
+
+    shared_array<unsigned char> copy_pixel_data(const SDL_Surface &src)
+    {
+        return shared_array<unsigned char>();
+    }
 }
 
 int main(int argc, char **argv)
@@ -47,13 +77,18 @@ int main(int argc, char **argv)
         cerr << "tics: " << "missing file operand" << endl;
         return 1;
     }
-    SDL_Surface *original = IMG_Load(argv[1]);
+    shared_ptr<SDL_Surface> original(IMG_Load(argv[1]), SDL_FreeSurface);
     if (original == 0) {
         cerr << "tics: " << SDL_GetError() << endl;
         return 1;
     }
     int width = original->w, height = original->h;
     if (!init_video(width, height)) {
+        return 1;
+    }
+    shared_ptr<SDL_Surface> original_32 = convert_surface_32(*original);
+    if (original_32 == 0) {
+        cerr << "tics: " << SDL_GetError() << endl;
         return 1;
     }
     srand(time(0));
