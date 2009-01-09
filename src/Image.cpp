c802931b3ec2d13a489b4827c9b8bd14dfa9d4ac
Mikael Lind <elemel@elemel.se>
2009-01-09 12:23:44 +0100

    use BOOST_FOREACH

--- src/Image.cpp
+++ src/Image.cpp
@@ -1,5 +1,6 @@
 #include "Image.hpp"
 #include "io.hpp"
+#include <boost/foreach.hpp>
 #include <GL/gl.h>
 
 using std::istream;
@@ -13,11 +14,9 @@ namespace tics {
     
     void Image::generate(int n)
     {
-        typedef vector<Triangle>::iterator Iterator;
-
         triangles_.resize(n);
-        for (Iterator i = triangles_.begin(); i != triangles_.end(); ++i) {
-            i->generate();
+        BOOST_FOREACH(Triangle &t, triangles_) {
+            t.generate();
         }
     }
     
@@ -32,38 +31,32 @@ namespace tics {
 
     void Image::draw() const
     {
-        typedef vector<Triangle>::const_iterator Iterator;
-
         glBegin(GL_TRIANGLES);
-        for (Iterator i = triangles_.begin(); i != triangles_.end(); ++i) {
-            i->draw();
+        BOOST_FOREACH(const Triangle &t, triangles_) {
+            t.draw();
         }
         glEnd();
     }
 
     void Image::read(istream &in)
     {
-        typedef vector<Triangle>::iterator Iterator;
-
         uint16_t n = 0;
         tics::read(in, width_);
         tics::read(in, height_);
         tics::read(in, n);
         triangles_.resize(n);
-        for (Iterator i = triangles_.begin(); i != triangles_.end(); ++i) {
-            i->read(in);
+        BOOST_FOREACH(Triangle &t, triangles_) {
+            t.read(in);
         }
     }
     
     void Image::write(ostream &out) const
     {
-        typedef vector<Triangle>::const_iterator Iterator;
-
         tics::write(out, width_);
         tics::write(out, height_);
         tics::write(out, uint16_t(triangles_.size()));
-        for (Iterator i = triangles_.begin(); i != triangles_.end(); ++i) {
-            i->write(out);
+        BOOST_FOREACH(const Triangle &t, triangles_) {
+            t.write(out);
         }
     }
 
--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -1,4 +1,5 @@
 #include "Triangle.hpp"
+#include <boost/foreach.hpp>
 
 using boost::array;
 using std::istream;
@@ -10,39 +11,31 @@ namespace tics {
 
     void Triangle::generate()
     {
-        typedef array<Vertex, 3>::iterator Iterator;
-        
         int r = rand() % 16, g = rand() % 16, b = rand() % 16, a = rand() % 8;
         int x = rand() % 256, y = rand() % 256;
-        for (Iterator i = vertices_.begin(); i != vertices_.end(); ++i) {
-            i->generate(r, g, b, a, x, y);
+        BOOST_FOREACH(Vertex &v, vertices_) {
+            v.generate(r, g, b, a, x, y);
         }
     }
 
     void Triangle::draw() const
     {
-        typedef array<Vertex, 3>::const_iterator Iterator;
-
-        for (Iterator i = vertices_.begin(); i != vertices_.end(); ++i) {
-            i->draw();
+        BOOST_FOREACH(const Vertex &v, vertices_) {
+            v.draw();
         }
     }
 
     void Triangle::read(istream &in)
     {
-        typedef array<Vertex, 3>::iterator Iterator;
-
-        for (Iterator i = vertices_.begin(); i != vertices_.end(); ++i) {
-            i->read(in);
+        BOOST_FOREACH(Vertex &v, vertices_) {
+            v.read(in);
         }
     }
     
     void Triangle::write(ostream &out) const
     {
-        typedef array<Vertex, 3>::const_iterator Iterator;
-
-        for (Iterator i = vertices_.begin(); i != vertices_.end(); ++i) {
-            i->write(out);
+        BOOST_FOREACH(const Vertex &v, vertices_) {
+            v.write(out);
         }
     }
 }
